2e66d250d03df1eb9d8bf396548deab4f2396378
ELYOR <elyorxalilov9201@gmail.com>
2024-02-12 18:35:49 +0500

    ADD

---
+++ src/Generic/Collection.java
@@ -0,0 +1,8 @@
+package Generic;
+
+public interface Collection<E> extends Itareble {
+
+    boolean add(E ad);
+    boolean remove(E remov);
+    boolean contains(E contain);
+}
---
+++ src/Generic/List.java
@@ -0,0 +1,8 @@
+package Generic;
+
+public interface List<E> extends Collection<E>{
+
+    boolean add(E ad);
+    boolean remove(E remov);
+    boolean contains(E contain);
+}
